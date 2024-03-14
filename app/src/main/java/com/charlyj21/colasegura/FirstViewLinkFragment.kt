d35181e9fb49536fcf83f98c8de925c3801fa89f
carlosjfh <carlos.flores@unas.edu.pe>
2024-03-14 00:24:38 -0500

    Connection stable bt to hc-05

--- app/src/main/java/com/charlyj21/colasegura/FirstViewLinkFragment.kt
+++ app/src/main/java/com/charlyj21/colasegura/FirstViewLinkFragment.kt
@@ -1,22 +1,28 @@
 package com.charlyj21.colasegura
 
+
+import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import android.Manifest
-import androidx.core.content.ContextCompat
+import java.io.IOException
+import java.util.UUID
 
 
 class FirstViewLinkFragment : Fragment() {
@@ -46,7 +52,8 @@ class FirstViewLinkFragment : Fragment() {
         if (requestCode == CODE_ON_BT) { // Comprobar si es la solicitud de Bluetooth
             if (resultCode == RESULT_OK) {
                 //test
-                connectToBluetooth()
+                //connectToBluetooth()
+                connectToBluetoothDevice()
             } else {
                 // Bluetooth no habilitado
                 Toast.makeText(requireContext(), "Bluetooth no habilitado", Toast.LENGTH_SHORT).show()
@@ -67,7 +74,8 @@ class FirstViewLinkFragment : Fragment() {
         if (requestCode == CODE_PM_BT) { // Comprobar si es la solicitud de Bluetooth
             if (resultCode == RESULT_OK) {
                 //test
-                connectToBluetooth()
+                //connectToBluetooth()
+                connectToBluetoothDevice()
             } else {
                 // Bluetooth no habilitado
                 Toast.makeText(requireContext(), "Permiso no habilitado", Toast.LENGTH_SHORT).show()
@@ -92,22 +100,57 @@ class FirstViewLinkFragment : Fragment() {
                 return
             }
 
-            // Solicitar habilitación de Bluetooth
+            //
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, CODE_ON_BT)
             return
         } else {
-            connectToBluetooth()
+            //connectToBluetooth()
+            connectToBluetoothDevice()
             return
         }
     }
 
     private fun connectToBluetooth(){
         findNavController().navigate(R.id.action_firstViewLinkFragment_to_secondViewLinkFragment)
-        //val intent = Intent(BluetoothDevice.ACTION_PAIRING_REQUEST)
-        //val bluetoothDevice = "98:D3:61:F5:C1:4A"
-        //intent.putExtra(BluetoothDevice.EXTRA_DEVICE, bluetoothDevice)
-        //intent.putExtra(BluetoothDevice.EXTRA_PAIRING_VARIANT, BluetoothDevice.PAIRING_VARIANT_PIN)
-        //startActivityForResult(intent, 2)
     }
+
+    private fun connectToBluetoothDevice() {
+        val bluetoothManager = getSystemService(requireContext(), BluetoothManager::class.java)
+        val bluetoothAdapter = bluetoothManager?.adapter
+
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                    CODE_PM_BT
+                )
+            }
+
+            //
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, CODE_ON_BT)
+        }
+
+        val deviceList = bluetoothAdapter?.bondedDevices
+        val specifiedDevice = deviceList?.find { it.name == "HC-05" }
+
+        // Crear un objeto BluetoothSocket para la conexión
+        val uuidString = "00001101-0000-1000-8000-00805F9B34FB"
+        val uuid = UUID.fromString(uuidString)
+        val socket: BluetoothSocket? = specifiedDevice?.createRfcommSocketToServiceRecord(uuid)
+
+        // Intentar conectar al dispositivo
+        try {
+            socket?.connect()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
 }
